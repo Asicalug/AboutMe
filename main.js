dc4b62c1c1379da1bcd287cc3e1f1cf59b04a9b2
asicalug <isaac.poissinger@gmail.com>
2023-10-13 07:25:28 +0200

    First Commit

---
+++ main.js
@@ -0,0 +1,9 @@
+$('.history-container').click(function() {
+    $('.history-text').css( {
+        'display': 'flex'
+    })
+})
+
+$('.navbar-projects').click(function() {
+    $(location).prop('href', 'test.html')
+}) 
